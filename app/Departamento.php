68beb02eec57a2bdc47f838a6ec18d631fd06187
CarlosP123 <carlopereyra@hotmail.com>
2022-10-28 01:01:35 -0300

    se crearon relaciones entre areas, departamentos, y empleados, a su vez, se creo el mcr de Departamento

--- app/Departamento.php
+++ app/Departamento.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Departamento extends Model
 {
-    //
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nombre', 'descripcion', 'area_id',
+    ];
+    
+    public function areas()
+    {
+        return $this->belongsTo('App\Area');
+    }
 }
--- app/Empleado.php
+++ app/Empleado.php
@@ -9,6 +9,10 @@ class Empleado extends Model
     public $timestamps = false;
 
     protected $fillable = [
-        'nombre', 'descripcion',
+        'nombre', 'descripcion','departamento_id',
     ];
+    public function departamentos()
+    {
+        return $this->belongsTo('App\Departamento');
+    }
 }
