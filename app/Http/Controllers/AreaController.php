75b310b7cf26d1f3e47f8b470cbf84e7859b0c2b
Carlos Emiliano Pereyra <44825099+CarloEmi@users.noreply.github.com>
2023-08-22 15:31:08 -0300

    Update

--- app/Http/Controllers/AreaController.php
+++ app/Http/Controllers/AreaController.php
@@ -10,24 +10,17 @@ class AreaController extends Controller
 {
     public function index()
     {
-        $areas = Area::all();
-
-        return view('empresa.areas.index', compact('areas'));
-    }
+        //
+         }
 
     public function crear()
     {
-        return view('empresa.areas.crear');
+        //
     }
 
     public function almacenar(AreaRequest $request)
     {
-        Area::create([
-            'nombre' =>$request->nombre,
-            'descripcion' =>$request->descripcion,
-            'empresa_id' =>1,
-        ]);
-        return redirect()->route('areas')->with('message', 'Registro creado exitosamente.');
+        //    
     }
 
     public function editar(Area $area)
@@ -37,8 +30,7 @@ class AreaController extends Controller
 
     public function actualizar(AreaRequest $request, Area $area)
     {
-        $area->update($request->all());
-        return redirect()->route('areas')->with('message', 'Registro modificado exitosamente');
+       //
     }
 }
 
